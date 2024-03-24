30c8382b63cc6e2d9e74f5f385c88092b3815b5e
Vladimir <81691626+medianyi@users.noreply.github.com>
2024-03-24 08:23:04 +0300

    Add Player and weapon
    Add Player and weapon sprite, script

---
+++ night nebula/Assets/Scriptes/Player.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+
+public class Player : MonoBehaviour
+{
+
+    public float speed;
+
+    private Rigidbody2D rb; 
+    private Animator anim;
+
+    private Vector2 moveAmount;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody2D>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 moveInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        moveAmount = moveInput.normalized * speed;
+
+        if (moveInput != Vector2.zero)
+        {
+            anim.SetBool("isRunning", true);
+        }
+        else
+        {
+            anim.SetBool("isRunning", false);
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + moveAmount * Time.fixedDeltaTime);
+    }
+}
